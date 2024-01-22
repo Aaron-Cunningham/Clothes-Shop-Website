8e1ee7a2d2898e070b8e494b5379aca0be15a904
Aaron-Cunningham <aaroncunningham222@gmail.com>
2024-01-22 18:57:05 +0000

    -Added router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
 
 import Nav from './Components/Nav';
 import './App.css';
-import Hero from './Components/Hero';
-import AddCard from './Components/AddCard';
+import Home from './Pages/Home';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import Login from './Pages/Login';
+import Tracking from './Components/Tracking';
+import ProgressBar from "@badrap/bar-of-progress";
+import { useEffect } from 'react';
+
+const progress = new ProgressBar({
+  size: 5,
+  color: "black",
+  className: "z-50",
+  delay: 10
+});
+
+function RouteChangeTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    progress.start();
+    setTimeout(() => {
+      progress.finish();
+    }, 500);
+  }, [location]);
+
+  return null;
+}
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Hero />
-      <AddCard/>
-    </div>
+    <Router>
+      <RouteChangeTracker />
+      <div className="App">
+        <Nav />
+        <div>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/tracking' element={<Tracking/>}/>
+        </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
