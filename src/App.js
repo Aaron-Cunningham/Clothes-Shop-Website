60a8e089b3789e1dc2b24be3c54530abd90766dc
Aaron-Cunningham <aaroncunningham222@gmail.com>
2024-01-22 20:14:40 +0000

    -Configured imports

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import './App.css';
 import Home from './Pages/Home';
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Login from './Pages/Login';
-import Tracking from './Components/Tracking';
+import Tracking from './Pages/Tracking';
 import ProgressBar from "@badrap/bar-of-progress";
 import { useEffect } from 'react';
+import Register from './Pages/Register';
+import Footer from './Components/Footer';
 
 const progress = new ProgressBar({
   size: 5,
@@ -32,15 +34,21 @@ function App() {
   return (
     <Router>
       <RouteChangeTracker />
-      <div className="App">
+      <div className="min-h-screen">
         <Nav />
         <div>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/login' element={<Login/>}/>
           <Route path='/tracking' element={<Tracking/>}/>
+          <Route path='register' element={<Register/>}/>
         </Routes>
         </div>
+        <div className='sticky top-[100vh]'>
+          <Footer/>
+
+        </div>
+        
       </div>
     </Router>
   );
