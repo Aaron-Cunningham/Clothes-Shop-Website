afc600622d48334e523d90de8eb8ecc20edf2c82
Aaron-Cunningham <aaroncunningham222@gmail.com>
2024-01-22 18:55:55 +0000

    -Moved

---
+++ src/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../Components/Hero'
+import AddCard from '../Components/Cards/AddMediumCard'
+
+function Home() {
+  return (
+    <div>
+        <Hero/>
+        <AddCard/>
+    </div>
+  )
+}
+
+export default Home
