b4b93c3f125d157bf8fcfee6beae63908d0a4b05
Aaron-Cunningham <aaroncunningham222@gmail.com>
2024-01-23 05:18:04 +0000

    -Updated routes to allow/disallow routes depedning on logged in status

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-
 import Nav from './Components/Nav';
 import './App.css';
 import Home from './Pages/Home';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation, Navigate } from 'react-router-dom';
 import Login from './Pages/Login';
 import Tracking from './Pages/Tracking';
 import ProgressBar from "@badrap/bar-of-progress";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Register from './Pages/Register';
 import Footer from './Components/Footer';
+import Axios from 'axios';
+import Account from './Pages/Account';
 
 const progress = new ProgressBar({
   size: 5,
@@ -31,30 +32,44 @@ function RouteChangeTracker() {
 }
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  //Session
+  Axios.defaults.withCredentials = true;
+
+
+  useEffect(() => {
+    Axios.defaults.withCredentials = true;
+    Axios.get("http://localhost:3001/loggedin").then((response) => {
+      console.log(response.data.loggedIn)
+      setLoggedIn(response.data.loggedIn);
+    });
+  }, []);
+  
+
+
+
+
+
   return (
     <Router>
       <RouteChangeTracker />
       <div className="min-h-screen">
-        <Nav />
+        <Nav loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>
         <div>
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/tracking' element={<Tracking/>}/>
-          <Route path='register' element={<Register/>}/>
+          <Route path='/' element={<Home />}/>
+          <Route path='/login' element={loggedIn ? <Navigate to="/account" /> : <Login setLoggedIn={setLoggedIn} />} />
+          <Route path='/tracking' element={<Tracking />}/>
+          <Route path='/register' element={loggedIn ? <Navigate to={"/account"}/> : <Register />}/>
+          <Route path='/account' element={<Account />}/>
         </Routes>
         </div>
         <div className='sticky top-[100vh]'>
-          <Footer/>
-
+          <Footer />
         </div>
-        
       </div>
     </Router>
   );
 }
 
-
 export default App;
-
-
