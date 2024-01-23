fded2ab80a7fc01d493054b081b1226365d4de1c
Aaron-Cunningham <aaroncunningham222@gmail.com>
2024-01-23 19:39:06 +0000

    -Update data file

---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const heroImg = [
+    {
+      id: 1,
+      img: "https://i.ibb.co/gz3wW47/1.jpg",
+      text: "Get summer ready"
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/5TMkbG8/2.jpg",
+      text: "Trending suits for men"
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/cxbNY5D/3.jpg",
+      text: "Trendy Business Attire"
+    }
+  ]
