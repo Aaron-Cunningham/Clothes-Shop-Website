7fd11939c137ef328162eac207a0e722cd0568b1
Aaron-Cunningham <aaroncunningham222@gmail.com>
2024-01-23 05:21:57 +0000

    -Update nav component to work with logged in/out users

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -7,7 +7,7 @@ import { BsPerson } from "react-icons/bs";
 import { Link } from 'react-router-dom';
 
 
-function Nav() {
+function Nav({loggedIn, setLoggedIn}) {
   const [open, setOpen] = useState(false); 
   const handleMenu = () => {
     setOpen(prev => !prev);
@@ -20,7 +20,7 @@ function Nav() {
     { title: "Jewelry", link: "/" },
   ];
   return (
-    <header className='shadow-lg flex top-0 pt-2 pb-2'>
+    <header className='shadow-lg flex top-0 pt-2 pb-2 z-100'>
       <div className='lg:grid grid-cols-3 flex-grow'>
         <div className='flex items-center justify-start px-10'>
           {/* Logo */}
@@ -29,43 +29,93 @@ function Nav() {
           </Link>
         </div>
 
-        <div className='hidden lg:flex items-center text-xl justify-center '>
+        <div className='hidden xl:flex items-center text-xl justify-center '>
           {/* Desktop Menu */}
           {navLinks.map((item, index) => (
-            <a className='a group active:scale-90 transition transform mx-16' key={index} href={item.link}><p className='p group-hover:p'>{item.title}</p></a>
+            <Link to={'/'} className='a group active:scale-90 transition transform mx-16'><p className='p group-hover:p'>{item.title}</p></Link>
           ))}
         </div>
+        
+        {loggedIn ? (
+            <div className='hidden lg:flex justify-end items-center space-x-5 pr-10'>
+            {/* Desktop Buttons */}
+              <Link to={'/basket'}>
+                <button className='hover:scale-110 transition transform active:scale-95 duration-200 ease-in-out'><MdOutlineShoppingBag className='h-6 w-6'/></button>
+              </Link>
+              <Link to={'/tracking'}>
+                <button className='hover:scale-110 transition transform active:scale-95 duration-200 ease-in-out'><FiTruck className='h-6 w-6'/></button>
+              </Link>
+  
+              <Link to='/account'>
+                <button className='hover:scale-110 transition transform active:scale-95 duration-200 ease-in-out'><BsPerson className='h-7 w-6'/></button>
+              </Link>
+              {loggedIn ? (
+          <div className='block transition-all duration-500 text-white bg-black hover:text-white px-3 py-2 rounded-md text-base font-medium cursor-pointer'>
+            <Link to="/" onClick={(e) => {setLoggedIn(false)}}><p>Logout</p></Link>
+          </div>
 
-        <div className='hidden lg:flex justify-end items-center space-x-5 pr-10'>
+        ) : ( <div></div>)}
+  
+          </div>
+        ) : (
+          <div className='hidden lg:flex justify-end items-center space-x-5 pr-10'>
           {/* Desktop Buttons */}
-            <Link to={'/tracking'}>
-              <button className='hover:scale-110 transition transform active:scale-95 duration-200 ease-in-out'><FiTruck className='h-6 w-6'/></button>
-            </Link>
-            <Link to='/login'>
-              <button className='hover:scale-110 transition transform active:scale-95 duration-200 ease-in-out'><BsPerson className='h-7 w-6'/></button>
-            </Link>
             <Link to={'/basket'}>
               <button className='hover:scale-110 transition transform active:scale-95 duration-200 ease-in-out'><MdOutlineShoppingBag className='h-6 w-6'/></button>
             </Link>
+            <Link to={'/tracking'}>
+              <button className='hover:scale-110 transition transform active:scale-95 duration-200 ease-in-out'><FiTruck className='h-6 w-6'/></button>
+            </Link>
+
+            <Link to='/login'>
+              <button className='hover:scale-110 transition transform active:scale-95 duration-200 ease-in-out'><BsPerson className='h-7 w-6'/></button>
+            </Link>
+
         </div>
+
+        )
+
+        }
+
 
       </div>
       
 
       {/* Mobile Menu Button */}
-      <div className='flex items-center px-10 cursor-pointer lg:hidden z-1'>
+      <div className='flex items-center px-10 cursor-pointer xl:hidden z-10'>
         <button type='button' onClick={handleMenu} className='z-10'>
-          <span className='sr-only'>Open main menu</span>
           {open ? <FaTimes className='w-8 h-8' /> : <FaBars className='w-8 h-8'/>}
         </button>
       </div>
 
       {/* Mobile Menu */}
       {open && (
-        <div className='absolute top-0 left-0 mt-14 bg-white w-full lg:hidden z-0'>
+        <div className='absolute top-0 left-0 mt-14 bg-white w-full lg:hidden z-100000'>
           {navLinks.map((item, index) => (
-            <a className='block transition-all duration-500 hover:bg-red-500 hover:text-white px-3 py-2 rounded-md text-base font-medium' key={index} href={item.link}>{item.title}</a>
+            <a className='block transition-all duration-500 hover:bg-black hover:text-white px-3 py-2 rounded-md text-base font-medium' key={index} href={item.link} >{item.title} </a>
           ))}
+
+        <div className='block transition-all duration-500 hover:bg-red-500 hover:text-white px-3 py-2 rounded-md text-base font-medium'>
+          <Link to="tracking"><p>Tracking</p></Link>
+
+        </div>
+        <div className='block transition-all duration-500 hover:bg-red-500 hover:text-white px-3 py-2 rounded-md text-base font-medium cursor-pointer'>
+          <Link to="/basket"><p>Basket</p></Link>
+        </div>
+        <div className='block transition-all duration-500 hover:bg-black hover:text-white px-3 py-2 rounded-md text-base font-medium'>
+          {loggedIn && (
+            <Link to="/account"><p>Account</p></Link>
+          )}
+        </div>
+        {loggedIn ? (
+          <div className='block transition-all duration-500 hover:bg-red-500 hover:text-white px-3 py-2 rounded-md text-base font-medium cursor-pointer'>
+            <Link to="/" onClick={(e) => {setLoggedIn(false)}}><p>Logout</p></Link>
+          </div>
+
+        ) : ( 
+          <div className='block transition-all duration-500 hover:bg-black hover:text-white px-3 py-2 rounded-md text-base font-medium'>
+            <Link to="/login"><p>Login</p></Link>
+          </div>)}
         </div>
       )}
     </header>
