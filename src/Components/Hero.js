c548373e57e3bf02fe6256dcd8bc466131c9ea20
Aaron-Cunningham <aaroncunningham222@gmail.com>
2024-01-23 05:18:23 +0000

    -Updated Hero CSS

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -3,31 +3,27 @@ import React from 'react'
 
 function Hero() {
   return (
-
-
-
-
         <section className='mt-1'>
             <div className='flex justify-evenly items-center bg-black text-center p-7 mb-1'>
-                <div className='hidden md:flex'>
+                <div className='hidden lg:flex'>
                     <button className='border-2 border-white text-white px-2 text-lg font-semibold active:scale-90 transition transform duration-500 ease-out w-[100px] hover:opacity-80'>WOMEN</button>
                 </div>
                 <div className=''>
                     <p className='text-white font-semibold'>Do you want 15% off almost everything?* Use code: WINTERMOKI</p>
                 </div>
-                <div className='hidden md:flex'>
+                <div className='hidden lg:flex'>
                     <button className='border-2 border-white text-white px-2 text-lg font-semibold active:scale-90 transition-all transform duration-500 ease-in-out w-[100px] hover:opacity-80'>MEN</button>
                 </div>
             </div>
             
             <div className='md:max-w-[85rem] md:mx-auto md:px-1 '>
 
-                <div className='relative h-[500px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px] '>
-                    <img src="https://robbreport.com/wp-content/uploads/2021/04/todd-snyder-footjoy-1.jpg" class="absolute w-full h-full object-cover -z-20"/>
+                <div className=' h-[500px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px] '>
+                    <img src="https://robbreport.com/wp-content/uploads/2021/04/todd-snyder-footjoy-1.jpg" class="w-full h-full object-cover"/>
 
-                    <div className='flex flex-col absolute top-1/4 left-0 right-0 mt-3 items-center'>
+                    <div className='flex flex-col absolute top-1/4 left-0 right-0 mt-12 lg:mt-3 items-center'>
                         <h1 className='text-white text-5xl md:text-5xl lg:text-7xl font-bold mb-5'>This is MOKI</h1>
-                        <div className='flex flex-col items-center justify-center mt-6 gap-7 text-2xl font-semibold md:flex-row'>
+                        <div className='flex flex-col items-center justify-center mt-6 gap-7 text-2xl font-semibold sm:flex-row'>
                             <button className='bg-white py-2 w-[180px] justify-center hover:opacity-80 transition-all transform duration-300 ease-in-out'>Shop Women</button>
                             <button className='bg-white py-2 w-[180px] flex justify-center hover:opacity-80 transition-all transform duration-300 ease-in-out'>Shop Men</button>
                         </div>
