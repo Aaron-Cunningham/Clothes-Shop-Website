0b947a426df1bac26d199c5c7abdacca25ce2622
Aaron-Cunningham <aaroncunningham222@gmail.com>
2024-01-23 22:22:56 +0000

    -Updated styling of medium cards

--- src/Components/Cards/AddMediumCard.js
+++ src/Components/Cards/AddMediumCard.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import MediumCard from './MediumCards';
-import { useBinData } from '../../useBinData';
+import { heroImg } from '../../data';
 
 function AddCard() {
-  const fetchedData = useBinData('65ae68d8dc746540189857a2')
+
+
+  const fetchedData = heroImg;
+  
   return (
     <section className='md:max-w-[85rem] md:mx-auto md:px-1'>
       <div className='flex flex-col gap-1 sm:flex-row sm:gap-1 mt-1'>
@@ -16,9 +19,7 @@ function AddCard() {
         ))}
       </div> 
       <div className="relative">
-        <div className="absolute top-0 left-0 right-0 bg-white text-center pb-5 sm:-mt-14 md:-mt-14">
-            <h1 className="hidden text-2xl font-semibold border-b-2 border-gray-400 sm:inline-block pb-1 md:text-3xl">Trending</h1>        
-        </div>
+
       </div>
     </section>
   );
