9f5f6da57e9e93f8f7113a39cf5bbc99abe26510
Aaron-Cunningham <aaroncunningham222@gmail.com>
2024-01-23 22:22:25 +0000

    -Updated Data

--- src/data.js
+++ src/data.js
@@ -15,3 +15,54 @@ export const heroImg = [
       text: "Trendy Business Attire"
     }
   ]
+
+  export const mensBannerImg = [
+
+
+    {
+      img: "https://i.ibb.co/Mgfhv8k/tet.webp",
+      title: 'T-Shirts',
+    },
+    {
+      img: "https://i.ibb.co/CzqzT79/2.webp",
+      title: 'Coats',
+    },
+    {
+      img: "https://i.ibb.co/GfDJjsg/3.webp",
+      title: 'Sports Wear',
+    },
+    {
+      img: 'https://i.ibb.co/h87Pwrd/4.jpg',
+      title: 'Hoodies',
+    },
+    {
+      img: "https://i.ibb.co/CBzmdmj/Jeans.webp",
+      title: "Trousers"
+    }
+  ]
+
+
+  export const womensBannerImg = [
+
+
+    {
+      img: "https://i.ibb.co/BB60b7G/11.jpg",
+      title: 'T-Shirts',
+    },
+    {
+      img: "https://i.ibb.co/R2S1GjM/22.webp",
+      title: 'Coats',
+    },
+    {
+      img: "https://i.ibb.co/GfDJjsg/3.webp",
+      title: 'Sports Wear',
+    },
+    {
+      img: 'https://i.ibb.co/kHFLTRB/44.webp',
+      title: 'Dresses',
+    },
+    {
+      img: "https://i.ibb.co/CBzmdmj/Jeans.webp",
+      title: "Jeans"
+    }
+  ]
