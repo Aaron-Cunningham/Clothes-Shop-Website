5aa6af7f82b0b1dff582fb2a1ece5559a469b03e
Aaron-Cunningham <aaroncunningham222@gmail.com>
2024-01-23 05:20:48 +0000

    -Implemented login form functionality to post to backend

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import Axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-function Login() {
+function Login({ setLoggedIn }) {
+    console.log("Login component rendered");
+    const [usernameLog, setUsernameLog] = useState('');
+    const [passwordLog, setPasswordLog] = useState('');
+    const [loginStatus, setLoginStatus] = useState('');
+
+    const loginUser = (e) => {
+        e.preventDefault();
+        Axios.post("http://localhost:3001/loggedin", {
+            username: usernameLog,
+            password: passwordLog,
+        }).then((response) => {
+            if (response.data.message) {
+                // User entered wrong credentials
+                console.log("Login failed:", response.data.message);
+                setLoginStatus(response.data.message);
+            } else {
+                // Correct credentials
+                setLoggedIn(true);
+            }
+        });
+    };
+
   return (
     <div className=' mb-10'>
         <div className='flex justify-center bg-gray-100 py-4'>
@@ -9,17 +32,20 @@ function Login() {
         </div>
         <div className='flex justify-center items-center mt-[3rem] md:mt-[15rem]'>
             <div className='w-[20rem] sm:w-[25rem]'>
-                <form className='w-full'>
+                <form className='w-full' onSubmit={loginUser}>
                 <div className='mb-8'>
-                    <input className='w-full border px-3 py-2 placeholder:text-gray-500 border-gray-400' placeholder='Email*'></input>
+                    <input className='w-full border px-3 py-2 placeholder:text-gray-500 border-gray-400' placeholder='Email*' onChange={(e) => {setUsernameLog(e.target.value)}}></input>
                 </div>
                 <div className='mb-2'>
-                    <input className='w-full border px-3 py-2 placeholder:text-gray-500 border-gray-400' placeholder='Password*'></input>
+                    <input className='w-full border px-3 py-2 placeholder:text-gray-500 border-gray-400' placeholder='Password*' type='password' autoComplete='current-password' onChange={(e) => {setPasswordLog(e.target.value)}}></input>
                 </div>
                 <div className=''>
-                    <Link to={'/forgot password'}>
-                        <p className='cursor-pointer underline hover:opacity-50 transition-all delay-100 mb-7'>Forgot your password?</p>
+                    <Link to={'/reset'}>
+                        <p className='cursor-pointer underline hover:opacity-50 transition-all delay-100 '>Forgot your password?</p>
                     </Link>
+                </div>
+                <div>
+                    <h1 className=' text-red-500 mb-5 mt-1'>{loginStatus}</h1>
                 </div>
                 <div className='mb-5'>
                     <button className='text-white bg-black w-full py-2 transform transition-all hover:opacity-80 active:scale-95 ease-in-out'>SIGN IN</button>
